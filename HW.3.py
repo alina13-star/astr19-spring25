8b6533dd7dd550a130bd534620c1cf9321058b99
alina13-star <asanc309@ucsc.edu>
2025-04-23 19:06:15 -0700

    Add files via upload
    Adding to journal for HW/3

---
+++ HW.3.py
@@ -0,0 +1,14 @@
+# This program will define
+# a function f(x) that returns 
+# x * 3 + 8
+
+# This function returns 
+def f(x):
+	return x *3 + 8
+def main():
+	x = 9
+	result = f(x)
+
+	if result > 27:
+		print("YAY!")
+main()
